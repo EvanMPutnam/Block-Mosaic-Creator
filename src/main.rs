df9a5ac03a72f6ffc05c3ba6d258ca0b4f356f3b
Evan Putnam <emput1998@gmail.com>
2025-05-26 01:41:57 -0600

    Updating algorithm to specify counts in the colors file for each count available

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::collections::HashMap;
+use std::cmp::Ordering;
 use std::env;
 use std::fs::File;
 use std::io::Read;
 use image::imageops::FilterType;
 use image::{GenericImageView, ImageReader};
 use nannou::prelude::*;
+use nannou::rand::prelude::SliceRandom;
+use nannou::rand::thread_rng;
 use serde::Deserialize;
 use serde_json;
 
@@ -12,20 +14,36 @@ const X_SIZE: u64 = 48;
 const Y_SIZE: u64 = 48;
 
 struct Model {
-    pixels: Colors
+    pixels: Vec<Color>,
 }
 
 #[derive(Debug, Deserialize)]
-struct Colors {
-    colors: Vec<Color>,
+struct ColorConfigs {
+    colors: Vec<ColorConfig>,
 }
 
 #[derive(Debug, Deserialize)]
-struct Color {
+struct ColorConfig {
     name: String,
     r: u8,
     g: u8,
     b: u8,
+    count: u64
+}
+
+impl ColorConfig {
+    fn decrement(&mut self) {
+        self.count -= 1;
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
+    x: u64,
+    y: u64
 }
 
 fn main() {
@@ -36,7 +54,7 @@ fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(BLACK);
     draw_square(&app, &draw, &model);
-    draw.to_frame(app, &frame).unwrap();
+    draw.to_frame(app, &frame).expect("Unable to draw to frame.");
 }
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {}
@@ -46,73 +64,78 @@ fn model(_app: &App) -> Model {
     if args.len() < 3 {
         panic!("Need to provide file paths for picture and color config")
     }
-    let picture_path = args.get(1).unwrap();
-    let color_data = args.get(2).unwrap();
+    let picture_path = args.get(1).expect("Please specify path to picture.");
+    let color_data = args.get(2).expect("Please specify path to color data.");
     if !color_data.ends_with(".json") {
         panic!("Need to provide filepath for color.json file")
     }
 
-    let img = ImageReader::open(picture_path).unwrap().decode();
-    let img_resized = img.unwrap().resize_exact(X_SIZE as u32, Y_SIZE as u32,
+    let img = ImageReader::open(picture_path)
+        .expect("Image failed to parse.")
+        .decode()
+        .expect("Failed to decode image.");
+    let img_resized = img.resize_exact(X_SIZE as u32, Y_SIZE as u32,
                                           FilterType::Nearest);
 
-    let mut file = File::open(color_data).unwrap();
+    let mut file = File::open(color_data).expect("Could not open color data file.");
     let mut buff = String::new();
-    file.read_to_string(&mut buff).unwrap();
+    file.read_to_string(&mut buff).expect("Unable to read color data file.");
 
-    let colors: Colors = serde_json::from_str(buff.as_str()).unwrap();
-    let mut color_arr: Vec<Color> = vec![];
+    let mut color_configs: ColorConfigs = serde_json::from_str(buff.as_str())
+        .expect("JSON not parseable.");
 
-    let mut used_colors: HashMap<String, u64> = HashMap::new();
-
+    let mut colors: Vec<Color> = Vec::new();
     for y in 0..Y_SIZE {
         for x in 0..X_SIZE {
             let pixel = img_resized.get_pixel(x as u32, (Y_SIZE - y - 1) as u32);
-            let closest_color = calculate_closest_color(&colors,
-                                                        pixel.0[0],
-                                                        pixel.0[1] as u8,
-                                                        pixel.0[2] as u8);
-            if closest_color == usize::MAX {
-                panic!("Need to provide at least 1 color to color json file.")
-            }
-
-            let color = colors.colors.get(closest_color).unwrap();
-            color_arr.push(Color {
-                name: color.name.clone(),
-                r: color.r,
-                g: color.g,
-                b: color.b,
-            });
-
-            if used_colors.get(color.name.as_str()).is_none() {
-                used_colors.insert(color.name.to_string(), 0);
-            }
-            used_colors.insert(color.name.to_string(),
-                               used_colors.get(color.name.as_str()).unwrap() + 1);
-
+            colors.push(Color{
+                r: pixel.0[0] as u8,
+                g: pixel.0[1] as u8,
+                b: pixel.0[2] as u8,
+                x,
+                y
+            })
         }
     }
+    colors.shuffle(&mut thread_rng());
 
-    let mut total = 0;
-    for (key, value) in used_colors.into_iter() {
-        println!("{} - {} pieces", key, value);
-        total += value
-    }
-    println!("Total Pieces: {}", total);
+    let mut colors: Vec<Color> = colors.iter().map(|original_color| -> Color {
+        let nearest_color = calculate_closest_color(&color_configs, original_color);
+        let selected_config = color_configs.colors
+            .get_mut(nearest_color)
+            .expect("Color configs should have value within index range");
+        selected_config.decrement();
+        Color {
+            r: selected_config.r,
+            g: selected_config.g,
+            b: selected_config.b,
+            x: original_color.x,
+            y: original_color.y
+        }
+    }).collect();
+    colors.sort_by(|a, b| match a.y.cmp(&b.y) {
+        Ordering::Equal => a.x.cmp(&b.x),
+        other => other,
+    });
 
     Model {
-        pixels: Colors { colors: color_arr },
+        pixels: colors,
     }
 }
 
-fn calculate_closest_color(colors: &Colors, r: u8, g: u8, b: u8) -> usize {
+fn calculate_closest_color(color_configs: &ColorConfigs, original_color: &Color) -> usize {
     let mut closest_dist: f32 = f32::MAX;
     let mut closest_index = usize::MAX;
     let mut count = 0;
-    for color in colors.colors.iter() {
-        let mut r_dist = (color.r as f32 - r as f32) * 0.3;
-        let mut g_dist = (color.g as f32 - g as f32) * 0.59;
-        let mut b_dist = (color.b as f32 - b as f32) * 0.11;
+    let mut has_available_color = false;
+    for color_config in color_configs.colors.iter() {
+        if color_config.count == 0 {
+            count += 1;
+            continue;
+        }
+        let mut r_dist = (color_config.r as f32 - original_color.r as f32) * 0.3;
+        let mut g_dist = (color_config.g as f32 - original_color.g as f32) * 0.59;
+        let mut b_dist = (color_config.b as f32 - original_color.b as f32) * 0.11;
         r_dist = r_dist * r_dist;
         g_dist = g_dist * g_dist;
         b_dist = b_dist * b_dist;
@@ -122,7 +145,11 @@ fn calculate_closest_color(colors: &Colors, r: u8, g: u8, b: u8) -> usize {
             closest_dist = dist;
             closest_index = count;
         }
+        has_available_color = true;
         count += 1;
+    }
+    if !has_available_color || closest_dist == f32::MAX {
+        panic!("Invalid configuration of colors.  Not enough colors present.")
     }
     closest_index
 }
@@ -143,7 +170,7 @@ fn draw_square(app: &App, draw: &Draw, model: &Model) {
         for x in 0..X_SIZE {
             let x_f: f32 = (x * x_width) as f32 - x_offset as f32;
             let y_f: f32 = (y * y_height) as f32 - y_offset as f32;
-            let color = model.pixels.colors.get(count).unwrap();
+            let color = model.pixels.get(count).expect("Model should have appropriate pixel count.");
             draw.rect().xy(Point2::new(x_f, y_f))
                 .color(srgb8(color.r, color.g, color.b))
                 .width(x_width as f32 - 1.0)
